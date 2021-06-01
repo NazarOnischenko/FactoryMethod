c2a76f6dc686d5164ba70b9a0c3357d57a6efcd0
NazarOnischenko <70802112+NazarOnischenko@users.noreply.github.com>
2021-06-02 01:46:23 +0300

    Add class Food and  DeliverOfFood

---
+++ FactoryMethod/DeliverOfFood.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class DeliverOfFood:Deliver
+    {
+        public DeliverOfFood(string name) : base(name) { }
+        public override Product ToDeliver()
+        {
+            return new Food(); 
+        }
+    }
+}
---
+++ FactoryMethod/Food.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class Food : Product
+    {
+        public Food() { Console.WriteLine("Доставка їжі"); }
+    }
+}
