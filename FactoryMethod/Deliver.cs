1bae231a9e46bf1fc006d3eb922977d96517eb5f
NazarOnischenko <70802112+NazarOnischenko@users.noreply.github.com>
2021-06-02 01:44:42 +0300

    Add Class Deliver

---
+++ FactoryMethod/Deliver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    abstract class Deliver
+    {
+        public string Name { get; set; }
+        public Deliver(string name)
+        {
+            Name = name;
+        }
+        abstract public Product ToDeliver(); 
+    }
+}
