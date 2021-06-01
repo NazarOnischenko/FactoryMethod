602d08d218b58ac0270d6c925bc4155fa8931753
NazarOnischenko <70802112+NazarOnischenko@users.noreply.github.com>
2021-06-02 01:50:08 +0300

    Add class Appliances and DeliverOfAppliances

---
+++ FactoryMethod/Appliances.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class Appliances:Product
+    {
+        public Appliances()
+        {
+            Console.WriteLine("Доставка побутової техніки");
+        }
+    }
+}
---
+++ FactoryMethod/DeliverOfAppliances.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class DeliverOfAppliances:Deliver
+    {
+        public DeliverOfAppliances(string name) : base(name) { }
+        public override Product ToDeliver()
+        {
+            return new Appliances();
+        }
+    }
+}
