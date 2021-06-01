33569bb7a269cefa08f7ae8682ecc02fabe940a7
NazarOnischenko <70802112+NazarOnischenko@users.noreply.github.com>
2021-06-02 01:48:01 +0300

    Add class Clothing and DeliverOfClothing

---
+++ FactoryMethod/Clothing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class Clothing:Product
+    {
+        public Clothing()
+        {
+            Console.WriteLine("Доставка одягу");
+        }
+    }
+}
---
+++ FactoryMethod/DeliverOfClothing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class DeliverOfClothing:Deliver
+    {
+        public DeliverOfClothing(string name):base(name)
+        {
+        }
+        public override Product ToDeliver() { return new Clothing(); }
+    }
+}
