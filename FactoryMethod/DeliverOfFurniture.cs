6932fa2d14dd6d9e1fa04fd5bc9be11a73435876
NazarOnischenko <70802112+NazarOnischenko@users.noreply.github.com>
2021-06-02 01:52:05 +0300

    Add class Furniture and DeliverOfFurniture

---
+++ FactoryMethod/DeliverOfFurniture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class DeliverOfFurniture:Deliver
+    {
+        public DeliverOfFurniture(string name) : base(name) { }
+        public override Product ToDeliver() { return new Furniture(); }
+
+    }
+}
---
+++ FactoryMethod/Furniture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class Furniture:Product
+    {
+        public Furniture()
+        {
+            Console.WriteLine("Доставка меблів");
+        }
+    }
+}
